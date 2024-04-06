878242f41bcf8a0d6815e1a22c3a01953cee65c1
Lysender <lysender@darkstar-win>
2024-04-06 23:54:30 +0800

    fix: more stuff

--- src/run.rs
+++ src/run.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::io::prelude::*;
+use std::io::BufReader;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use tracing::info;
@@ -9,6 +10,8 @@ use crate::worker::WorkerPool;
 pub fn run() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     let worker_pool = WorkerPool::new(4);
+
+    info!("Server started on port 3000");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -22,28 +25,35 @@ pub fn run() {
 }
 
 fn handle_connect(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-
-    // Log the first line
-    let lines: Vec<String> = String::from_utf8_lossy(&buffer[..])
+    let reader = BufReader::new(&stream);
+    let request: Vec<_> = reader
         .lines()
-        .map(|x| x.to_string())
+        .map(|x| x.unwrap())
+        .take_while(|x| !x.is_empty())
         .collect();
 
-    info!("{}", lines[0]);
+    let Some(first_line) = request.first() else {
+        return handle_invalid_request(stream);
+    };
 
-    // Basic routing capability
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
+    info!("{}", first_line);
 
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else if buffer.starts_with(sleep) {
-        std::thread::sleep(std::time::Duration::from_secs(5));
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let route_parts: Vec<&str> = first_line.split_whitespace().collect();
+    let Some(method) = route_parts.get(0) else {
+        return handle_invalid_request(stream);
+    };
+
+    let Some(uri) = route_parts.get(1) else {
+        return handle_invalid_request(stream);
+    };
+
+    let (status_line, filename) = match *uri {
+        "/" => ("HTTP/1.1 200 OK", "index.html"),
+        "/sleep" => {
+            std::thread::sleep(std::time::Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     handle_response(stream, status_line, filename);
@@ -54,6 +64,17 @@ fn handle_response(mut stream: TcpStream, status_line: &str, filename: &str) {
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
         status_line,
+        contents.len(),
+        contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn handle_invalid_request(mut stream: TcpStream) {
+    let contents = fs::read_to_string("400.html").unwrap();
+    let response = format!(
+        "HTTP/1.1 400 BAD REQUEST\r\nContent-Length: {}\r\n\r\n{}",
         contents.len(),
         contents
     );
